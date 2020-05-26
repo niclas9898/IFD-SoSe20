bcf924a7b1e2a564bb7b05a9dbdefcedc3539247
Niclas Christ <46569626+niclas9898@users.noreply.github.com>
2020-05-26 12:30:42 +0200

    f

--- VUI/js/playgroud-artyom-script.js
+++ VUI/js/playgroud-artyom-script.js
@@ -8,13 +8,7 @@ window.addEventListener("load", function () {
         }
     });
 
-    var commandHello = {
-        indexes:["Hallo"], // These spoken words will trigger the execution of the command
-        action:function(){ // Action to be executed when a index match with spoken word
-            artyom.say("Guten Morgen");
-        }
-    };
-    artyom.addCommands(commandHello);
+
 
 
     function startContinuousArtyom() {
