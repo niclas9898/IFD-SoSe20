d4fd4e2b90595353e528e46dc53cad3f8ac1bf22
Niclas Christ <46569626+niclas9898@users.noreply.github.com>
2020-06-05 11:35:49 +0200

    Update playgroud-artyom-script.js

--- VUI/js/playgroud-artyom-script.js
+++ VUI/js/playgroud-artyom-script.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function () {
 
 
     var commandHello = {
-        indexes:["Guten Morgen"], // These spoken words will trigger the execution of the command
+        indexes:["Guten Morgen", "hallo"], // These spoken words will trigger the execution of the command
         action:function(){ // Action to be executed when a index match with spoken word
             artyom.say("Willkommen Ihr Kaffee steht bereit. Reden wir über die gestrigen Zahlen?");
            
@@ -74,48 +74,297 @@ window.addEventListener("load", function () {
     artyom.addCommands(Ausschuss)
 
 
-    
-    var myGroup = [
-        {
-            description:"If my database contains the name of a person say something",
-            smart:true, // a Smart command allow you to use wildcard in order to retrieve words that the user should say
-            // Ways to trigger the command with the voice
-            indexes:["Do you know who is *","I don't know who is *","Is * a good person"],
-            // Do something when the commands is triggered
-            action:function(i,wildcard){
-                var database = ["Carlos","Bruce","David","Joseph","Kenny"];
-    
-                //If the command "is xxx a good person" is triggered do, else
-                if(i == 2){
-                    if(database.indexOf(wildcard.trim())){
-                        artyom.say("I'm a machine, I dont know what is a feeling");
-                    }else{
-                        artyom.say("I don't know who is " + wildcard + " and i cannot say if is a good person");
-                    }
-                }else{
-                    if(database.indexOf(wildcard.trim())){
-                        artyom.say("Of course i know who is "+ wildcard + ". A really good person");
-                    }else{
-                        artyom.say("My database is not big enough, I don't know who is " + wildcard);
-                    }
-                }
-            }
-        },
-        {
-            indexes:["What time is it","Is too late"],
-            action:function(i){ // var i returns the index of the recognized command in the previous array
-                if(i == 0){
-                    aFunctionThatSaysTheTime(new Date());
-                }else if(i == 1){
-                    artyom.say("Never is too late to do something my friend !");
-                }
-            }
+
+
+
+
+
+    var Stück1 = {
+        indexes:["Wie hoch sind die Produktionszahlen für Werk 1", "Wie viel wurde in Werk 1 produziert"], 
+        action:function(){ 
+            artyom.say("In Werk 1 wurden bereits 2543 Stück produziert");
+           
         }
-    ];
+
     
-    artyom.addCommands(myGroup); 
+
+    };
+
+    artyom.addCommands(Stück1)
+    var Stück2 = {
+        indexes:["Wie hoch sind die Produktionszahlen für Werk 2", "Wie viel wurde in Werk 2 produziert"], 
+        action:function(){ 
+            artyom.say("In Werk 1 wurden bereits 5967 Stück produziert");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(Stück2)
+    var Stück3 = {
+        indexes:["Wie hoch sind die Produktionszahlen für Werk 3", "Wie viel wurde in Werk 3 produziert"], 
+        action:function(){ 
+            artyom.say("In Werk 1 wurden bereits 3958 Stück produziert");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(Stück3)
+    var Stück4 = {
+        indexes:["Wie hoch sind die Produktionszahlen für Werk 4", "Wie viel wurde in Werk 4 produziert"], 
+        action:function(){ 
+            artyom.say("In Werk 1 wurden bereits 5734 Stück produziert");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(Stück4)
+                    
+        
+           
+
+           
+    var krank1 = {
+        indexes:["Wie viele Mitarbeiter sind in Werk 1 krank","Wie viele Krankmeldungen gibt es in Werk 1" ], 
+        action:function(){ 
+            artyom.say("In Werk 1 sind heute 3 Mitarbeiter und Mitarbeiterinnen krank");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(krank1)
+    var krank2 = {
+        indexes:["Wie viele Mitarbeiter sind in Werk 2 krank","Wie viele Krankmeldungen gibt es in Werk 2"], 
+        action:function(){ 
+            artyom.say("In Werk 1 sind heute 2  Mitarbeiter und Mitarbeiterinnen  krank");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(krank2)
+    var krank3 = {
+        indexes:["Wie viele Mitarbeiter sind in Werk 3 krank","Wie viele Krankmeldungen gibt es in Werk 3"], 
+        action:function(){ 
+            artyom.say("In Werk 1 sind heute 0  Mitarbeiter und Mitarbeiterinnen  krank");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(krank3)
+    var krank4 = {
+        indexes:["Wie viele Mitarbeiter sind in Werk 4 krank","Wie viele Krankmeldungen gibt es in Werk 4"], 
+        action:function(){ 
+            artyom.say("In Werk 1 sind heute 70  Mitarbeiter und Mitarbeiterinnen krank. Möchten Sie mehr Infos zu den Krankmeldungen?");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(krank4)
+    var krank41 = {
+        indexes:["mehr infos zu den Krankmeldungen"], 
+        action:function(){ 
+            artyom.say("Der plötzliche Anstieg an Krankmeldungen in Werk 4 ist sehr verdächtig. Bitte verordnen Sie dringend alle Mitarbeiter und Mitarbeiterinnen an COVID-19 Tests durchzuführen");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(krank41)
+   
+  
 
    
+
+           
+    var ausschuss1 = {
+        indexes:["Wie hoch liegt der Ausschuss in Werk 1" ], 
+        action:function(){ 
+            artyom.say("Der Ausschuss in Werk 1 liegt bei 400 stück");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(ausschuss1)
+    var ausschuss2 = {
+        indexes:["Wie hoch liegt der Ausschuss in Werk 2"], 
+        action:function(){ 
+            artyom.say("Der Ausschuss in Werk 2 liegt bei 30 stück");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(ausschuss2)
+    var ausschuss3 = {
+        indexes:["Wie hoch liegt der Ausschuss in Werk 3"], 
+        action:function(){ 
+            artyom.say("Der Ausschuss in Werk 3 liegt bei 34 stück");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(ausschuss3)
+    var ausschuss4 = {
+        indexes:["Wie hoch liegt der Ausschuss in Werk 4"], 
+        action:function(){ 
+            artyom.say("Der Ausschuss in Werk 4 liegt bei 40 stück");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(ausschuss4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var Verschleiß1 = {
+        indexes:["Wie hoch sind die Kosten für den Verschleiß in Werk 1", "Verschleiß Werk 1" ], 
+        action:function(){ 
+            artyom.say("Die Kosten für den Verschleiß in Werk 1 liegen bei 1700€. Wollen Sie mehr Infos dazu?");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(Verschleiß1)
+
+    var Verschleiß11 = {
+        indexes:["Bitte mehr Infos zum Verschleiß in Werk 1" ], 
+        action:function(){ 
+            artyom.say("Maschine 4 an Band 12 ist seit gestern defekt. Es wird sich bereits darum gekümmert. Die Produktion wurde eingestellt");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(Verschleiß11)
+
+
+    var Verschleiß2 = {
+        indexes:["Wie hoch sind die Kosten für den Verschleiß in Werk 2", "Verschleiß Werk 2"], 
+        action:function(){ 
+            artyom.say("Die Kosten für den Verschleiß in Werk 2 liegen bei 500€");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(Verschleiß2)
+    var Verschleiß3 = {
+        indexes:["Wie hoch sind die Kosten für den Verschleiß in Werk 3", "Verschleiß Werk 3"], 
+        action:function(){ 
+            artyom.say("Die Kosten für den Verschleiß in Werk 3 liegen bei 500€");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(Verschleiß3)
+    var Verschleiß4 = {
+        indexes:["Wie hoch sind die Kosten für den Verschleiß in Werk 4", "Verschleiß Werk 4"], 
+        action:function(){ 
+            artyom.say("Die Kosten für den Verschleiß in Werk 4 liegen bei 500€");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(Verschleiß4)
+
+
+
+
+
+
+    var meldung1 = {
+        indexes:["Was sind die wichtigsten Meldungen des Tages?"], 
+        action:function(){ 
+            artyom.say("Der Ausschuss in Werk 1 ist durch einen Fehler in der Maschine gestern deutlich gestiegen, wodurch auch die Kosten für den Verschließ in Werk 1 über dem Durchschnitt liegen. Die Produktion wurde eingestellt. Fahren wir fort?");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(meldung1)
+
+    var meldung2 = {
+        indexes:[" weiter"], 
+        action:function(){ 
+            artyom.say("Der plötzliche Anstieg an Krankmeldungen in Werk 4 ist sehr verdächtig. Bitte verordnen Sie dringend alle Mitarbeiter an COVID-19 Tests durchzuführen. Das waren die wichtigsten Meldungen des Tages");
+           
+        }
+
+    
+
+    };
+
+    artyom.addCommands(meldung2)
+
+
+
+
+
+    
+
 
 
 
